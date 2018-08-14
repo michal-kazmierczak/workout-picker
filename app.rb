6f187f58a46efa91334da1766b2f03d1aa7cc2fe
Michał Kaźmierczak <michalkazmierczak@icloud.com>
2018-08-14 10:19:56 +0200

    add '0.0.0.0' so that it works in a container

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ require 'sinatra'
 require_relative 'lib/knapsack_solver'
 require_relative 'lib/input_output_mappers'
 
+set :bind, '0.0.0.0'
+
 # the main entrypoint
 # run 'ruby app.rb' and visit localhost:4567 to see the result
 post '/' do
@@ -15,7 +17,7 @@ post '/' do
   capacity = data['time']
   items = map_exercises_to_knapsack_problem(data['exercises'])
   picked_items = KnapsackSolver.exec(capacity, items)
-  
+
   map_picked_items_to_result(picked_items, data['exercises']).to_json
 rescue JSON::ParserError, InvalidInputError
   halt 422, { message: 'malformed body' }.to_json
